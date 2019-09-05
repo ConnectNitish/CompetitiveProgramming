ccddf93527a44a0fd1f80804bcf3304eb9f4ce66
ConnectNitish <17176938+ConnectNitish@users.noreply.github.com>
2019-09-05 14:46:02 +0530

    DFS Topological Sorting

--- CodeFiles/Graph/topological_sort_graph_tutorial_dfs.cpp
+++ CodeFiles/Graph/topological_sort_graph_tutorial_dfs.cpp
@@ -22,68 +22,68 @@ void dfs_topo_sort(int src)
 		if(visited[ng]==false)
 			dfs_topo_sort(ng);
 
-	nodes_in_topo.push(src);
-}
+		nodes_in_topo.push(src);
+	}
 
-void dfs_helper()
-{
-	vector<int> topo_sort;
-	
-	for(auto a:visited)
+	void dfs_helper()
 	{
-		if(a.second==false)
+		vector<int> topo_sort;
+		
+		for(auto a:visited)
 		{
-			dfs_topo_sort(a.first);
-
-			while(nodes_in_topo.empty()==false)
+			if(a.second==false)
 			{
-				topo_sort.push_back(nodes_in_topo.top());
-				nodes_in_topo.pop();
+				dfs_topo_sort(a.first);
 			}
 		}
+
+		while(nodes_in_topo.empty()==false)
+		{
+			topo_sort.push_back(nodes_in_topo.top());
+			nodes_in_topo.pop();
+		}
+
+		if(debug)
+			cout << " DFS : Gives Topo Sort But not Guaranteed Lexicographiccaly Smallest " << endl;
+
+		for(auto a:topo_sort)
+		{
+			cout << a << " ";
+		}
+
+		if(debug)
+			cout<<endl;
+
 	}
 
-	if(debug)
-		cout << " DFS : Gives Topo Sort But not Guaranteed Lexicographiccaly Smallest " << endl;
 
-	for(auto a:topo_sort)
+	int main()
 	{
-		cout << a << " ";
-	}
-
-	if(debug)
-		cout<<endl;
-
-}
-
-
-int main()
-{
-	if(debug)
-	{
-		freopen("input.txt","r",stdin);
-		freopen("output.txt","w",stdout);
-	}
-	int v,e;
-	cin >> v >> e;
+		if(debug)
+		{
+			freopen("input.txt","r",stdin);
+			freopen("output.txt","w",stdout);
+		}
+		int v,e;
+		cin >> v >> e;
 
 	// because graph is 1 based index 
-	for(int i=1;i<=v;i++)
-	{
-		visited[i] = false;
-		graph[i] = {};
-		indegree_count[i]=0;
-	}
+		for(int i=1;i<=v;i++)
+		{
+			visited[i] = false;
+			graph[i] = {};
+			indegree_count[i]=0;
+		}
 
-	for(int i=0;i<e;i++)
-	{
-		int st,ed;
-		cin >> st >> ed;
-		graph[st].insert(ed);
+		for(int i=0;i<e;i++)
+		{
+			int st,ed;
+			cin >> st >> ed;
+			graph[st].insert(ed);
 		// graph[ed].insert(st);
-		indegree_count[ed]++;
+			indegree_count[ed]++;
+		}
+
+		dfs_helper();
+
 	}
-
-	dfs_helper();
-
-}
