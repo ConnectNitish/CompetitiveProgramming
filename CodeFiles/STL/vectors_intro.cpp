86fadd2539c98fca70a57b4e175ca79f2a9f61ae
priyalHedau15 <69321329+priyalHedau15@users.noreply.github.com>
2020-10-01 20:11:27 +0530

    Updated vectors_intro.cpp
    Added concept of vectors of vectors

--- CodeFiles/STL/vectors_intro.cpp
+++ CodeFiles/STL/vectors_intro.cpp
@@ -47,4 +47,35 @@ int main(){
 	cout<<d.size()<<endl;
 	cout<<d.capacity()<<endl; //size of underlying array
 	cout<<d.max_size()<<endl; //max no of element a vector can hold in the wrost case acc to avbl memory in system
+	
+	
+	//vectors of vectors
+	vector<vector<int> > vec; 
+  
+    // Elements to insert in column 
+    int num = 10; 
+  
+    // Inserting elements into vector 
+    for (int i = 0; i < ROW; i++) { 
+        // Vector to store column elements 
+        vector<int> v1; 
+  
+        for (int j = 0; j < COL; j++) { 
+            v1.push_back(num); 
+            num += 5; 
+        } 
+  
+        // Pushing back above 1D vector 
+        // to create the 2D vector 
+        vec.push_back(v1); 
+    } 
+  
+    // Displaying the 2D vector 
+    for (int i = 0; i < vec.size(); i++) { 
+        for (int j = 0; j < vec[i].size(); j++) 
+            cout << vec[i][j] << " "; 
+        cout << endl; 
+    } 
+	
+	return 0;
 }
